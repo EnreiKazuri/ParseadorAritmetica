d36f1735676a9236e4596467c162c9ef0246ca4c
Muirghen <vasquezmariano01@gmail.com>
2022-11-17 11:46:03 -0400

    Add project files.

---
+++ ParseadorAritmetica/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Antlr4.Runtime;
+
+namespace ParseadorAritmetica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "5*6+9*3-1";
+            Console.WriteLine("Input: {0}\n", input);
+            ICharStream stream = CharStreams.fromString(input);
+            aritmeticaLexer lexer = new aritmeticaLexer(stream);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            aritmeticaParser parser = new aritmeticaParser(tokens);
+            aritmeticaParser.AritmeticaContext tree  = parser.aritmetica();
+            Aritmetica aritmetica = new Aritmetica();
+            int res = aritmetica.Visit(tree);
+        }
+    }
+}
